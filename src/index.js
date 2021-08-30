7702965081b54ae15bd60a92dbbf4e782a6a1059
frandrew90 <frandrewjs@gmail.com>
2021-08-31 01:08:28 +0300

    made markup of menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles.css';
+
+import data from './menu.json';
+
+import makeMarkup from './template.hbs';
+
+console.log(data);
+
+const listRefs = document.querySelector('.js-menu');
+console.log(listRefs);
+
+console.log(makeMarkup);
+
+const markup = makeMarkup(data);
+
+// listRefs.insertAdjacentHTML('afterbegin', markup);
+listRefs.innerHTML = markup;
