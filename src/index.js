6b609d260da805db257edfeb6c38c520afe9d4ea
frandrew90 <frandrewjs@gmail.com>
2021-08-31 16:16:29 +0300

    delted console.logs

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import data from './menu.json';
 
 import makeMarkup from './template.hbs';
 
-console.log(data);
-
 const listRefs = document.querySelector('.js-menu');
-console.log(listRefs);
-
-console.log(makeMarkup);
 
 const markup = makeMarkup(data);
 
